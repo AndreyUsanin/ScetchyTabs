216003f1436d37b59649c986ce8d4d4163a2105c
rvwdude <rvwdude@gmail.com>
2019-04-25 11:17:14 +0300

    tabbytabs

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,29 +1,4 @@
 import "../styles/index.scss";
 
 // TABS
-
-window.onload = function() {
-  // Variables
-  var tabLinks = document.querySelectorAll(".tabs__link");
-  var tabContents = document.querySelectorAll(".tabs__item");
-
-  // Loop through the tab link
-  for (var i = 0; i < tabLinks.length; i++) {
-    tabLinks[i].addEventListener("click", function(e) {
-      e.preventDefault();
-      var id = this.hash.replace("#", "");
-
-      // Loop through the tab content
-      for (var j = 0; j < tabContents.length; j++) {
-        var tabContent = tabContents[j];
-        tabContent.classList.remove("is-visible");
-        tabLinks[j].classList.remove("is-active");
-        if (tabContent.id === id) {
-          tabContent.classList.add("is-visible");
-        }
-      }
-
-      this.classList.add("is-active");
-    });
-  }
-};
+var tabs = new Tabby('[data-tabs]');
