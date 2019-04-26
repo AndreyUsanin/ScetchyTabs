fe3d256f980af94bdc1054a5ccf068fc16336bed
rvwdude <rvwdude@gmail.com>
2019-04-26 10:34:53 +0300

    accordion

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,16 @@
 import "../styles/index.scss";
 
-// TABS
-var tabs = new Tabby('[data-tabs-v]');
+
+import BadgerAccordion from 'badger-accordion';
+
+// Creating a new instance of the accordion
+// const accordionDomNode = document.querySelector('.js-badger-accordion');
+
+// const accordion = new BadgerAccordion(accordionDomNode);
+
+new BadgerAccordion('.accordion', {
+  openHeadersOnLoad: [0],
+  headerClass: '.accordion__header',
+  panelClass: '.accordion__panel',
+  panelInnerClass: '.accordion__inner'
+});
